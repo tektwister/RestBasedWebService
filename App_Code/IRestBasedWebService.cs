c39af7d2fade0ff64a9b2bd19d6726927e2c19eb
Aravind Raj <aravinddevaraj777@gmail.com>
2019-05-11 15:33:05 +0530

    Initial commit

---
+++ App_Code/IRestBasedWebService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IRestBasedWebService
+{
+    [OperationContract]
+    [WebGet(UriTemplate="/welcome/{myname}")]  //the name specified in UriTemplate must match the parameter name eg.. myname
+    string Welcome(string myname);
+
+    [OperationContract]
+    [WebGet(UriTemplate = "/add/{val1}/{val2}")]
+    int Add(string val1, string val2);  //arguments for methods must be strings bcoz Variables for UriTemplate path segments must have type 'string'.
+
+}
---
+++ App_Code/RestBasedWebService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class RestBasedWebService : IRestBasedWebService
+{
+    public string Welcome(string yourName)
+    {
+
+        return string.Format("Welcome to WCF Web Services with REST and XML, {0}!",yourName);
+    }
+    public int Add(string val1, string val2)
+    {
+        int sum = Int32.Parse(val1) + Int32.Parse(val2);
+        return sum;
+    }
+}
